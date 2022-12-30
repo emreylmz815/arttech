ccdd463fd732ed70c4bb889ce7c87526ee03b116
Emre Yılmaz <emreylmz815@gmail.com>
2022-12-30 11:52:04 +0300

    fourt commit

--- src/pages/Home/HomeAbout/HomeAbout.js
+++ src/pages/Home/HomeAbout/HomeAbout.js
@@ -25,13 +25,13 @@ const HomeAbout = () => {
                            <span>ARTTECH KİMDİR?</span>
                            <h2>Sanat ile Teknolojinin Buluşma Noktası</h2>
                         </div>
-                        <p>Arttech Güvenlik Sistemleri, 15 yıllık piyasa tecrübesi sonucunda 2008 yılında kurulmuş olup, CCTV, alarm sistemlerinin ithalatını ve toptan satışını yapan bir kuruluştur.
-
-Firmamızın öncelikli amacı her sektörde faaliyet gösteren firmaların ihtiyaçlarını teklif verme aşamasından başlayarak, sağlıklı bir sistem oluşturulması için çözümler sunmaktır.</p>
+                        <p>Arttech Güvenlik Hizmetleri A.Ş., 20 yıllık sektörel tecrübeye sahip olan ve teknoloji alanında faaliyet gösteren profesyonel güvenlik sistemleri şirketidir.
+Arttech'in öncelikli amacı her sektörde faaliyet gösteren firmaların ihtiyaçlarını projelendirme aşamasından başlayarak, günümüz teknolojisi ile buluşturmaktır. Ülke genelinde geniş servis ağına sahip olan Arttech, mutlu müşteri ve kaliteli hizmet anlayışını prensip edinmiştir.</p>
                         <div className="about__list">
                            <ul>
                               <li ><span><i > <FaCheck/> </i>İnovatif Fikirler</span></li>
                               <li ><span><i > <FaCheck/> </i>Profesyonel Ekipler</span></li>
+                              <li ><span><i > <FaCheck/> </i>Takım Çalışması</span></li>
                               <li ><span><i > <FaCheck/> </i>Mutlu Müşteriler</span></li>
                            </ul>
                         </div>
