3a04dbb49cbb38910b85bae1b862304fbc84da1a
Emre Yılmaz <emreylmz815@gmail.com>
2023-01-24 00:43:54 +0300

    change

--- src/components/shared/PageHelmet.js
+++ src/components/shared/PageHelmet.js
@@ -4,14 +4,19 @@ import { Helmet } from "react-helmet";
 const PageHelmet = ({ pageTitle }) => {
     return (
         <>
+      
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>{pageTitle} || ARTTECH GÜVENLİK</title>
-                <meta name="robots" content="noindex, follow" />
-                <meta name="description" content="ARTTECH GÜVENLİK" />
+                <meta name="robots" content="index,follow" />
+                <meta name="author" content="ARTTECH GÜVENLİK" />
+                <meta name="description" content="ARTTECH GÜVENLİK, Akıllı Ev Sistemleri, Profesyonel Kamera Güvenlik Sistemleri, Personel Takip Sistemleri, Yangın Algılama ve Hırsız Alarm Sistemleri, Network Çözümleri" />
+                <meta name="keywords" content="Akıllı Ev, Güvenlik Kamera Sistemleri, Personel Takip Sistemleri, Yangın Algılama ve Hırsız Alarm Sistemleri, Düzce Akıllı Ev, Bolu Akıllı Ev, Zonguldak Akıllı Ev, plaka tanıma sistemleri"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             </Helmet>
         </>
+  
+
     );
 };
 
